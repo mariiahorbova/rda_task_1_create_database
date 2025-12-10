41a0d2fa3b6437829ffcfff0d02c349a08e625cc
mariiahorbova <gorbova21@gmail.com>
2025-12-10 14:33:25 +0200

    fix: add IF NOT EXISTS to create tables

--- task.sql
+++ task.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS ShopDB;
 USE ShopDB;
 
 -- Create Products table
-CREATE TABLE Products IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS Products (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Name VARCHAR(50) NOT NULL,
     Description VARCHAR(100),
@@ -12,7 +12,7 @@ CREATE TABLE Products IF NOT EXISTS (
 );
 
 -- Create Customers table
-CREATE TABLE Customers IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS Customers (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     FirstName VARCHAR(50) NOT NULL,
     LastName VARCHAR(50) NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE Customers IF NOT EXISTS (
 );
 
 -- Create Orders table
-CREATE TABLE Orders IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS Orders (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     CustomerID INT,
     Date DATE NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE Orders IF NOT EXISTS (
 );
 
 -- Create OrderItems table
-CREATE TABLE OrderItems IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS OrderItems (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     OrderID INT,
     ProductID INT,
